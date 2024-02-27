7def7c13d3ca9cfd36f44adb0e4d16c41df0329c
Abbas Uddin <105127468+CodeAbbas@users.noreply.github.com>
2024-02-27 23:09:24 +0000

    Update script.js

--- script.js
+++ script.js
@@ -52,25 +52,7 @@ const particles = Particles.init({
   ]
 });
 // background1
-const particles = Particles.init({
-      selector: ".background1"
-    });
-  };
-  const particles = Particles.init({
-    selector: ".background1",
-    color: ["#03dac6", "#ff0266", "#000000"],
-    connectParticles: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        options: {
-          color: ["#faebd7", "#03dac6", "#ff0266"],
-          maxParticles: 43,
-          connectParticles: false
-        }
-      }
-    ]
-  });
+
 
 class NavigationPage {
   constructor() {
