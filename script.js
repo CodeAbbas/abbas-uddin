7dae808446888b535dff4bc1632dc386e19f8e0e
Abbas Uddin <105127468+CodeAbbas@users.noreply.github.com>
2024-06-21 12:33:14 +0100

    Update script.js
    Particles js responsive

--- script.js
+++ script.js
@@ -30,31 +30,7 @@ function zeroPadding(num, digit) {
     return (zero + num).slice(-digit);
 }
 // CLOCK
-/*
-window.onload = function () {
-  Particles.init({
-    selector: ".background"
-  });
-  Particles.init({
-    selector: ".background1"
-  });
-};
-const particles = Particles.init({
-  selector: ".background",
-  color: ["#03dac6", "#ff0266", "#000000"],
-  connectParticles: true,
-  responsive: [
-    {
-      breakpoint: 768,
-      options: {
-        color: ["#faebd7", "#03dac6", "#ff0266"],
-        maxParticles: 43,
-        connectParticles: false
-      }
-    }
-  ]
-});*/
-window.onload = function () {
+window.onload = function() {
     Particles.init({
     selector: ".background1",
     color: ["#03dac6", "#ff0266", "#000000"],
